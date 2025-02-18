84fd95042bbc2e169e17d13baa8fbfc81ace6807
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-02-18 18:14:27 +0530

    deploymen

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  reactStrictMode: true,
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
